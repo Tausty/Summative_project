1a87817b922518b4988af89bf55bd3737cb52a64
Taust <116812445+Tausty@users.noreply.github.com>
2023-01-26 13:51:43 +0000

    added check for color manipulation

--- main/code/Mainmenu.cpp
+++ main/code/Mainmenu.cpp
@@ -1,5 +1,5 @@
 #include <ncurses.h>
-#include<cstdlib>
+
 
 
 
@@ -9,31 +9,18 @@ int main (int argc, char ** argv)
 {
 	initscr();
 	cbreak();
-	noecho();
-	int height,width,starty,startx;
-	height = 10;
-	starty = 10;
-	startx = 10;
-	width = 20;
-	WINDOW * win = newwin(height,width,starty,startx);
-	refresh();
+	if(!has_colors){
+		printw("this terminal does not support colors, please find one that does");
+		getch();
+		return -1;
+	}
+	if(!can_change_color){
+		printw("this terminal does not support changing colors. Please find one that does ");
+		getch();
+		return -1;
+	}
 
-	box (win,0,0);
-	int left,right,top,bottem,tlc,trc,blc,brc;
-	left=right=top=bottem=tlc=trc=blc=brc=102;
-	wborder(win,left,right,top,bottem,tlc,trc,blc,brc);
-	mvwprintw(win,2,5,"option 1");
-	wrefresh(win);
-	mvwprintw(win,5,5,"option 2");
-	wrefresh(win);
-	int c = getch();
-	if (c==int('1')) {
-		mvwprintw(win,2,3,">");
-	}
-	else {
-		mvwprintw(win,5,3,">");
-	}
-	wrefresh(win);
+
 
 	getch();
 	endwin();
